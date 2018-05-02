04f9073c5dde99f56335edcdcb0df5ed0cd5409e
IIDreams <1039173845@qq.com>
2018-05-02 09:18:44 +0800

    Login

--- WhatieSDKDemo/Login/ViewController.m
+++ WhatieSDKDemo/Login/ViewController.m
@@ -47,17 +47,16 @@
 
 - (IBAction)loginAction:(id)sender {
     
-//    [self POSTTEST];
-//    return;
-    
 
-    NSString *email = @"15207136550@163.com";
+//    NSString *email = @"15207136550@163.com";
 //    NSString *email = @"zhouwei20150901@icoud.com";
 //    NSString *email = @"huqintest";
-//NSString *email = @"whatieTest0002";
-//    NSString *email = self.emailTextField.text;
-//    NSString *password = [EHOMEExtensions MD5EncryptedWith:self.passwordTextField.text];
-    NSString *password = [EHOMEExtensions MD5EncryptedWith:@"123456789"];
+//    NSString *email = @"whatieTest0002";
+//    NSString *password = [EHOMEExtensions MD5EncryptedWith:@"123456789"];
+    
+    NSString *email = self.emailTextField.text;
+    NSString *password = [EHOMEExtensions MD5EncryptedWith:self.passwordTextField.text];
+    
     
     if ([email length] > 0 && [password length] > 0) {
         [EHOMEUserModel loginWithEmail:email password:password startBlock:^{
@@ -72,14 +71,6 @@
             [HUDHelper hideAllHUDsForView:sharedKeyWindow animated:YES];
             
             [HUDHelper addHUDInView:sharedKeyWindow text:NSLocalizedString(@"LoginSuccess", nil) hideAfterDelay:1.0];
-            
-            //        [EHOMEUserModel updateLoginPasswordWithEmail:@"zhouwei20150901@icloud.com" OldPasswordMD5:@"123456" newPasswordMD5:[EHOMEExtensions MD5EncryptedWith:@"123456"] startBlock:^{
-            //            NSLog(@"开始修改密码");
-            //        } successBlock:^(id responseObject) {
-            //            NSLog(@"修改密码成功 = %@", responseObject);
-            //        } failBlock:^(NSError *error) {
-            //            NSLog(@"修改密码失败 = %@", error);
-            //        }];
             
             [[EHOMEMQTTClientManager shareInstance] loginMQTT];
             
@@ -101,56 +92,6 @@
 
 }
 
--(void)POSTTEST{
-    
-    NSString *email = @"zhouwei20150901@icloud.com";
-    
 
-    
-    NSDictionary *params = @{@"phone":@(76182),           @"password":[EHOMEExtensions MD5EncryptedWith:@"123456"]};
-    
-    
-
-    
-    NSURL *url = [NSURL URLWithString:@"https://app.ceks100.com/server/login/loginAdvisor"];
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
-    request.HTTPMethod = @"POST";
-
-    
-    NSString *str = [self HTTPBodyWithParameters:params];
-    
-    request.HTTPBody = [str dataUsingEncoding:NSUTF8StringEncoding];
-    
-    
-    NSURLSession *session = [NSURLSession sharedSession];
-    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-        // Do sth to process returend data
-        if(!error)
-        {
-            NSLog(@"%@", [NSJSONSerialization JSONObjectWithData:data options:0 error:nil]);
-        }
-        else
-        {
-            NSLog(@"%@", error.localizedDescription);
-        }
-    }];
-    [dataTask resume];
-}
-
-- (NSString *)HTTPBodyWithParameters:(NSDictionary *)parameters
-{
-    NSMutableArray *parametersArray = [[NSMutableArray alloc]init];
-    
-    for (NSString *key in [parameters allKeys]) {
-        id value = [parameters objectForKey:key];
-        if (value != nil) {
-            [parametersArray addObject:[NSString stringWithFormat:@"%@=%@",key,value]];
-        }else{
-            [parametersArray addObject:[NSString stringWithFormat:@"%@=%@",key,@""]];
-        }
-    }
-    
-    return [parametersArray componentsJoinedByString:@"&"];
-}
 
 @end
